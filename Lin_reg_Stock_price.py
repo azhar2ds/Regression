7f45ea9d65913833456f2b23cf9c74325ab3a281
Azharuddin Mohammed <46964996+azhar2ds@users.noreply.github.com>
2020-03-12 17:39:26 +0530

    Linear Regression Stock Price Prediction
    Simple and Multiple Linear Regression for Stock Price Prediction

---
+++ Lin_reg_Stock_price.py
@@ -0,0 +1,73 @@
+from pandas import DataFrame
+from sklearn import linear_model
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+  
+Stock_Market = {'Year': [2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016],
+                'Month': [12, 11,10,9,8,7,6,5,4,3,2,1,12,11,10,9,8,7,6,5,4,3,2,1],
+                'Interest_Rate': [2.75,2.5,2.5,2.5,2.5,2.5,2.5,2.25,2.25,2.25,2,2,2,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75],
+                'Unemployment_Rate': [5.3,5.3,5.3,5.3,5.4,5.6,5.5,5.5,5.5,5.6,5.7,5.9,6,5.9,5.8,6.1,6.2,6.1,6.1,6.1,5.9,6.2,6.2,6.1],
+                'Stock_Index_Price': [1464,1394,1357,1293,1256,1254,1234,1195,1159,1167,1130,1075,1047,965,943,958,971,949,884,866,876,822,704,719]        
+                }
+ 
+df = DataFrame(Stock_Market,columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price'])
+ 
+plt.scatter(df['Interest_Rate'], df['Stock_Index_Price'], color='red')
+plt.title('Stock Index Price Vs Interest Rate', fontsize=14)
+plt.xlabel('Interest Rate', fontsize=14)
+plt.ylabel('Stock Index Price', fontsize=14)
+plt.grid(True)
+plt.show()
+print('\n\nseaborn visual\n\n\n')
+sns.pairplot(df,x_vars=['Interest_Rate','Unemployment_Rate'],y_vars=['Stock_Index_Price'],size=5,aspect=0.9,kind='reg')
+
+plt.scatter(df['Unemployment_Rate'], df['Stock_Index_Price'], color='green')
+plt.title('Stock Index Price Vs Unemployment Rate', fontsize=14)
+plt.xlabel('Unemployment Rate', fontsize=14)
+plt.ylabel('Stock Index Price', fontsize=14)
+plt.grid(True)
+plt.show()
+'''
+As you can see, a linear relationship exists in both cases:
+
+In the first case, when interest rates go up, the stock index price also goes up
+In the second case, when unemployment rates go up, the stock index price goes down (here we still have a linear relationship, but with a negative slope)
+'''
+
+print('\n\n\nPerforming the Multiple Linear Regression using StatsModels.api\n\n\n')
+
+
+Stock_Market = {'Year': [2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016],
+                'Month': [12, 11,10,9,8,7,6,5,4,3,2,1,12,11,10,9,8,7,6,5,4,3,2,1],
+                'Interest_Rate': [2.75,2.5,2.5,2.5,2.5,2.5,2.5,2.25,2.25,2.25,2,2,2,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75],
+                'Unemployment_Rate': [5.3,5.3,5.3,5.3,5.4,5.6,5.5,5.5,5.5,5.6,5.7,5.9,6,5.9,5.8,6.1,6.2,6.1,6.1,6.1,5.9,6.2,6.2,6.1],
+                'Stock_Index_Price': [1464,1394,1357,1293,1256,1254,1234,1195,1159,1167,1130,1075,1047,965,943,958,971,949,884,866,876,822,704,719]        
+                }
+
+df = DataFrame(Stock_Market,columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price'])
+
+X = df[['Interest_Rate','Unemployment_Rate']] # here we have 2 variables for multiple regression. If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.Alternatively, you may add additional variables within the brackets
+Y = df['Stock_Index_Price']
+ 
+# with sklearn
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+print('Intercept: \n', regr.intercept_)
+print('\nCoefficients: \n', regr.coef_)
+print('\nScore: \n', regr.score(X,Y))
+
+# prediction with sklearn
+New_Interest_Rate = 2.75
+New_Unemployment_Rate = 5.3
+print ('\nPredicted Stock Index Price: \n', regr.predict([[New_Interest_Rate ,New_Unemployment_Rate]]))
+
+# with statsmodels
+X = sm.add_constant(X) # adding a constant
+ 
+model = sm.OLS(Y, X).fit()
+predictions = model.predict(X) 
+ 
+print_model = model.summary()
+print(print_model)
