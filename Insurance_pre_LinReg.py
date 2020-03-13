178742f58388a18e2b971fbefbdfc5d1a232fa93
Azharuddin Mohammed <46964996+azhar2ds@users.noreply.github.com>
2020-03-13 21:22:51 +0530

    Swedish Insurance Prediction_Linear Regression
    Insurance claims prediction_Linear Regression

---
+++ Insurance_pre_LinReg.py
@@ -0,0 +1,213 @@
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+
+d=pd.read_excel('C:/Users/Azhar/Downloads/Swedish_Insurance.xls')
+x=pd.DataFrame(d['X'])
+y=list(d['Y'])
+print(d.head())
+
+#sns.pairplot(d,x_vars=['X'], y_vars=['Y'],height=5,aspect=0.9, kind='reg')
+l=LinearRegression()
+l.fit(x,y)
+print('^'*80)
+print('Coeffficient:',l.coef_)
+print('Intercept:',l.intercept_)
+print('5th Value prediction:',l.predict(x[57:58]))
+print('Actual Values:',y[58])
+
+print('^'*80)
+
+#calculating mean
+print('Mean of x:',tuple(np.mean(x))) # we can use tuple or list or np array
+print('Mean of x:',tuple(np.sum(x)/len(x)))# we can use tuple or list or np array
+print('Mean of x:',tuple(np.sum(x) / len(x)))# we can use tuple or list or np array
+print('^'*80)
+print('Mean of y:',np.mean(y)) # we can use tuple or list or np array
+print('Mean of y:',np.sum(y)/len(y))# we can use tuple or list or np array
+print('Mean of y:',np.sum(y) / len(y))# we can use tuple or list or np array
+print('^'*80)
+
+ #sum squared difference for each value from the mean value
+#variance = sum((x-list(np.mean(x)))**2)
+#print(variance)
+print('^'*80)
+
+# Calculate the mean value of a list of numbers
+def mean(values):
+	return sum(values) / float(len(values))
+ 
+# Calculate the variance of a list of numbers
+def variance(values, mean):
+	return sum([(x-mean)**2 for x in values])
+ 
+# calculate mean and variance
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+x = [row[0] for row in dataset]
+y = [row[1] for row in dataset]
+mean_x, mean_y = mean(x), mean(y)
+var_x, var_y = variance(x, mean_x), variance(y, mean_y)
+print('x stats: mean=%.3f variance=%.3f' % (mean_x, var_x))
+print('y stats: mean=%.3f variance=%.3f' % (mean_y, var_y))
+
+print('^'*80)
+
+def covariance(x, mean_x, y, mean_y):
+	covar = 0.0
+	for i in range(len(x)):
+		covar += (x[i] - mean_x) * (y[i] - mean_y)
+	return covar
+
+
+
+# Calculate Covariance
+
+# Calculate the mean value of a list of numbers
+def mean(values):
+	return sum(values) / float(len(values))
+
+# Calculate covariance between x and y
+def covariance(x, mean_x, y, mean_y):
+	covar = 0.0
+	for i in range(len(x)):
+		covar += (x[i] - mean_x) * (y[i] - mean_y)
+	return covar
+
+# calculate covariance
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+x = [row[0] for row in dataset]
+y = [row[1] for row in dataset]
+mean_x, mean_y = mean(x), mean(y)
+covar = covariance(x, mean_x, y, mean_y)
+print('Covariance: %.3f' % (covar))
+
+print('^'*80)
+
+# Calculate coefficients
+def coefficients(dataset):
+	x = [row[0] for row in dataset]
+	y = [row[1] for row in dataset]
+	x_mean, y_mean = mean(x), mean(y)
+	b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+	b0 = y_mean - b1 * x_mean
+	return [b0, b1]
+
+# Calculate coefficients
+def coefficients(dataset):
+	x = [row[0] for row in dataset]
+	y = [row[1] for row in dataset]
+	x_mean, y_mean = mean(x), mean(y)
+	b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+	b0 = y_mean - b1 * x_mean
+	return [b0, b1]
+
+
+
+# Calculate Coefficients
+print('^'*80)
+# Calculate the mean value of a list of numbers
+def mean(values):
+	return sum(values) / float(len(values))
+
+# Calculate covariance between x and y
+def covariance(x, mean_x, y, mean_y):
+	covar = 0.0
+	for i in range(len(x)):
+		covar += (x[i] - mean_x) * (y[i] - mean_y)
+	return covar
+
+# Calculate the variance of a list of numbers
+def variance(values, mean):
+	return sum([(x-mean)**2 for x in values])
+print('^'*80)
+# Calculate coefficients
+def coefficients(dataset):
+	x = [row[0] for row in dataset]
+	y = [row[1] for row in dataset]
+	x_mean, y_mean = mean(x), mean(y)
+	b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+	b0 = y_mean - b1 * x_mean
+	return [b0, b1]
+
+# calculate coefficients
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+b0, b1 = coefficients(dataset)
+print('Coefficients: B0=%.3f, B1=%.3f' % (b0, b1))
+
+
+
+def simple_linear_regression(train, test):
+	predictions = list()
+	b0, b1 = coefficients(train)
+	for row in test:
+		yhat = b0 + b1 * row[0]
+		predictions.append(yhat)
+	return predictions
+
+print('^'*80)
+
+# Standalone simple linear regression example
+from math import sqrt
+
+# Calculate root mean squared error
+def rmse_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		prediction_error = predicted[i] - actual[i]
+		sum_error += (prediction_error ** 2)
+	mean_error = sum_error / float(len(actual))
+	return sqrt(mean_error)
+
+# Evaluate regression algorithm on training dataset
+def evaluate_algorithm(dataset, algorithm):
+	test_set = list()
+	for row in dataset:
+		row_copy = list(row)
+		row_copy[-1] = None
+		test_set.append(row_copy)
+	predicted = algorithm(dataset, test_set)
+	print(predicted)
+	actual = [row[-1] for row in dataset]
+	rmse = rmse_metric(actual, predicted)
+	return rmse
+
+# Calculate the mean value of a list of numbers
+def mean(values):
+	return sum(values) / float(len(values))
+
+# Calculate covariance between x and y
+def covariance(x, mean_x, y, mean_y):
+	covar = 0.0
+	for i in range(len(x)):
+		covar += (x[i] - mean_x) * (y[i] - mean_y)
+	return covar
+
+# Calculate the variance of a list of numbers
+def variance(values, mean):
+	return sum([(x-mean)**2 for x in values])
+
+# Calculate coefficients
+def coefficients(dataset):
+	x = [row[0] for row in dataset]
+	y = [row[1] for row in dataset]
+	x_mean, y_mean = mean(x), mean(y)
+	b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+	b0 = y_mean - b1 * x_mean
+	return [b0, b1]
+
+# Simple linear regression algorithm
+def simple_linear_regression(train, test):
+	predictions = list()
+	b0, b1 = coefficients(train)
+	for row in test:
+		yhat = b0 + b1 * row[0]
+		predictions.append(yhat)
+	return predictions
+
+# Test simple linear regression
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+rmse = evaluate_algorithm(dataset, simple_linear_regression)
+print('RMSE: %.3f' % (rmse))
+
